071736e9923308ac55b73ce2f2080839b25b10dc
Gabry17 <gbianchi528@gmail.com>
2022-07-13 14:44:44 +0200

    aggiunto not found con ritorno alla home

---
+++ resources/js/pages/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    NOT FOUNDDD
+    <router-link :to="{ name: 'home' }">Torna alla HOME</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'NotFound'
+}
+</script>
+
+<style>
+
+</style>
--- resources/js/router.js
+++ resources/js/router.js
@@ -6,6 +6,7 @@ Vue.use(VueRouter);
 import Home from './pages/Home.vue';
 import About from './pages/About.vue';
 import Blog from './pages/Blog.vue';
+import NotFound from './pages/NotFound';
 
 const router = new VueRouter(
     {
@@ -25,6 +26,10 @@ const router = new VueRouter(
                 path: '/blog',
                 name: 'blog',
                 component: Blog
+            },
+            {
+                path: '/*',
+                component: NotFound
             }
         ]
     }
